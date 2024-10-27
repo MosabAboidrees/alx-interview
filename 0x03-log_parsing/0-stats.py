e389c62d058a7d2bae7f4c61a50dd0ffb05bfbc2
MOSAB ABOIDREES ALTRAIFI YOUSIF <33727845+MosabAboidrees@users.noreply.github.com>
2024-10-27 22:31:08 +0300

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,58 +1,45 @@
 #!/usr/bin/python3
-"""
-Log Parsing Script
-"""
 
+"""Script that reads stdin line by line and computes metrics"""
 
 import sys
 
 
-def print_stats(statistics: dict, total_file_size: int) -> None:
-    """Prints the statistics of HTTP status codes and total file size."""
-    # Print total file size
-    print("File size: {:d}".format(total_file_size))
-    # Print each status code count if it has a non-zero value
-    for code, count in sorted(statistics.items()):
-        if count:
-            print("{}: {}".format(code, count))
+def printsts(dic, size):
+    """ WWPrints information """
+    print("File size: {:d}".format(size))
+    for i in sorted(dic.keys()):
+        if dic[i] != 0:
+            print("{}: {:d}".format(i, dic[i]))
 
 
-if __name__ == '__main__':
-    # Initialize total file size and line count
-    total_file_size, line_count = 0, 0
+sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+       "404": 0, "405": 0, "500": 0}
 
-    # Define valid HTTP status codes to track
-    valid_c = ["200", "301", "400", "401", "403", "404", "405", "500"]
-    # Initialize status code counts to zero
-    statistics = {code: 0 for code in valid_c}
+count = 0
+size = 0
 
-    try:
-        # Read each line from standard input
-        for line in sys.stdin:
-            line_count += 1  # Increment line count
-            # Split the line into components based on whitespace
-            data = line.split()
-            try:
-                # Extract the status code (second-to-last item in the line)
-                status_code = data[-2]
-                # If the status code is one we track, increment its count
-                if status_code in statistics:
-                    statistics[status_code] += 1
-            except IndexError:
-                # Skip lines that do not have enough data
-                pass
-            try:
-                # Add the file size (last item in the line) to the total
-                total_file_size += int(data[-1])
-            except (IndexError, ValueError):
-                # Skip lines that do not have a valid file size
-                pass
-            # Print stats every 10 lines
-            if line_count % 10 == 0:
-                print_stats(statistics, total_file_size)
-        # Print final stats after processing all lines
-        print_stats(statistics, total_file_size)
-    except KeyboardInterrupt:
-        # Print stats when interrupted
-        print_stats(statistics, total_file_size)
-        raise
+try:
+    for line in sys.stdin:
+        if count != 0 and count % 10 == 0:
+            printsts(sts, size)
+
+        stlist = line.split()
+        count += 1
+
+        try:
+            size += int(stlist[-1])
+        except ValueError:
+            pass
+
+        try:
+            if stlist[-2] in sts:
+                sts[stlist[-2]] += 1
+        except IndexError:
+            pass
+    printsts(sts, size)
+
+
+except KeyboardInterrupt:
+    printsts(sts, size)
+    raise

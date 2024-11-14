358d02aaf8bd0d5544ab06a90b1e26c67aa22d0c
MosabAboidrees <mosab_mahala@yahoo.com>
2024-11-14 22:17:29 +0300

    Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+
 const request = require('request');
 const movieId = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
@@ -6,17 +7,17 @@ const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 // Fetch the movie data
 request(url, async (err, response, body) => {
     if (err) {
-      console.log(err);
+        console.log(err);
     }
     for (const characterId of JSON.parse(body).characters) {
         await new Promise((resolve, reject) => {
-          request(characterId, (err, response, body) => {
-            if (err) {
-              reject(err);
-            }
-            console.log(JSON.parse(body).name);
-            resolve();
-          });
+            request(characterId, (err, response, body) => {
+                if (err) {
+                    reject(err);
+                }
+                console.log(JSON.parse(body).name);
+                resolve();
+            });
         });
-      }
-    });
+    }
+});
